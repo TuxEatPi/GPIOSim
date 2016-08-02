0e536d653da290beb50514cb7c4faca77aed86d1
Thibault Cohen <thibault.cohen@nuance.com>
2016-08-02 11:54:14 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 setup(
-    name='tuxeatpi',
+    name='GPIOSim',
+    packages=find_packages(),
     entry_points="""
     [console_scripts]
     GPIOSim=GPIOSim.gui:main
